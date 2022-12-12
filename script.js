24cf516c81a2bf11340d9684d324690a3568e908
mtanng9 <mlt152498@gmail.com>
2022-12-11 23:26:50 -0500

    redid features with window prompts instead of web API

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 // Assignment code here
 function generatePassword() {
+  var passwordLength = window.prompt("How long would you like the password to be? (8-128 characters)").trim();
+  if (passwordLength === "") {
+    return "No inputs were entered for the password length";
+  } 
+
+  if (isNaN(passwordLength)) {
+    return "You did not input a number";
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    return "You entered a value that is either less than 8 or greater than 128";
+  }
+
+  var lowercase = window.prompt("Do you want lowercase letters? (yes/no)").trim();
+  var uppercase = window.prompt("Do you want uppercase letters? (yes/no)").trim();
+  var numeric = window.prompt("Do you want numbers? (yes/no)").trim();
+  var specialCharacters = window.prompt("Do you want special characters? (yes/no)").trim();
+  
+  if (lowercase != "yes" && uppercase != "yes" && numeric != "yes" && specialCharacters!= "yes") { 
+    return "Nothing has been entered for the character types";
+  }
+   
   return "test";
 }
-
-function recordInput(){
-  var passwordLength = document.querySelector("#password-length").value;
-  if (passwordLength === "") {
-    console.log("No inputs were entered for the password length");
-  } 
-  if (passwordLength < 8 || passwordLength > 128) {
-    console.log("You entered a value that is either less than 8 or greater than 128");
-  }
-
-  var characterTypes = document.querySelectorAll('#character-types option:checked');
-  var selectedValues = Array.from(characterTypes).map(el => el.value);
-  if (selectedValues.length === 0) {
-    console.log("Nothing has been checked for the character types");
-  }
-
-  console.log(passwordLength);
-  console.log(selectedValues);
-}
-
-var submitBtn = document.querySelector("#submit");
-submitBtn.addEventListener("click", recordInput);
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
