e905f13b1123f70341ca25bafc6535c40c09768b
mtanng9 <mlt152498@gmail.com>
2022-12-11 21:17:13 -0500

    Added validations logic and password criteria box

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ function generatePassword() {
   return "test";
 }
 
+function recordInput(){
+  var passwordLength = document.querySelector("#password-length").value;
+  if (passwordLength === "") {
+    console.log("No inputs were entered for the password length");
+  } 
+  if (passwordLength < 8 || passwordLength > 128) {
+    console.log("You entered a value that is either less than 8 or greater than 128");
+  }
+
+  var characterTypes = document.querySelectorAll('#character-types option:checked');
+  var selectedValues = Array.from(characterTypes).map(el => el.value);
+  if (selectedValues.length === 0) {
+    console.log("Nothing has been checked for the character types");
+  }
+
+  console.log(passwordLength);
+  console.log(selectedValues);
+}
+
+var submitBtn = document.querySelector("#submit");
+submitBtn.addEventListener("click", recordInput);
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
